eeee0c6bd61d49e8b1042e2b7cee504ce1ee493c
GFHAA <pprogers.xyz@gmail.com>
2022-07-15 16:23:40 +0300

    files

---
+++ spinWords.js
@@ -0,0 +1,12 @@
+function spinWords(string) {
+    let arr = string.split(" ");
+    let arr2 = arr.map(function(el){
+        if(el.lenght >= 5){
+            
+        }else{
+            console.log(2)
+        }
+    })
+
+}
+spinWords("Welcome")
